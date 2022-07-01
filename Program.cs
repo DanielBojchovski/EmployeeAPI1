fe4b3e549a4ab9752aba9348b21398e8871e2dc9
Daniel Bojchovski <106997266+DanielBojchovski@users.noreply.github.com>
2022-07-02 00:10:29 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using EmployeeAPI1.Models;
+using EmployeeAPI1.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<IEmpRepository, EmployeeRepository>();
+builder.Services.AddDbContext<EmployeeContext>(options =>
+    options.UseSqlite("Data source=employees.db"));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
